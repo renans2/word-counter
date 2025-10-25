e997c8fb2aeaffb8acb411c7b422d1aa655228d4
Renan Silva <renansupermanox@gmail.com>
2025-10-25 18:08:40 +0100

    added components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import WordCounterProvider from "./context/WordCounterProvider";
+import { ThemeProvider } from "styled-components";
+import { dark } from "./styles/themes/dark";
+import { light } from "./styles/themes/light";
+import { GlobalStyles } from "./styles/global";
+import Header from "./components/Header";
+import type { Theme } from "./types/Theme";
+import Main from "./components/Main";
 
 export default function App() {
+  const [theme, setTheme] = useState<Theme>("light");
 
   return (
-    <>
+    <ThemeProvider theme={theme === "dark" ? dark : light}>
+      <GlobalStyles />
       <WordCounterProvider>
-        <h1>test</h1>
+        <Header 
+          theme={theme} 
+          toggleTheme={() => setTheme(prev => prev === "dark" ? "light" : "dark")}
+        />
+        <Main />
       </WordCounterProvider>
-    </>
+    </ThemeProvider>
   );
 }
